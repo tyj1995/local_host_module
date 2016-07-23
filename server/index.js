c95576e1815805ddb32fd690b381cc7db0ffca18
Jessica Park <jpark2973@gmail.com>
2016-07-23 15:28:29 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+"use strict"
+require('dotenv').config();
+const express = require('express');
+const compression = require('compression');
+
+let port = process.env.PORT;
+let app = express();
+
+app.listen(port);
+console.log('listening on port: ', port);
+
+app.use(compression());
+app.use(express.static(__dirname + '/../.'));
+
+module.exports =  app;
